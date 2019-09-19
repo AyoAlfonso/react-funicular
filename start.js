561c1e8eb5f873b98d0636eeac92102d4719b533
AyoAlfonso <alfonsoayo7@gmail.com>
2019-09-19 12:18:51 +0100

    Spread operator

---
+++ start.js
@@ -0,0 +1,13 @@
+/* Situation 1 */
+const east = ["Uganda", "Kenya", "Tanzania"]; 
+console.log(...east);
+
+//Expected Result:  "Uganda", "Kenya", "Tanzania"
+
+/* Situation 2 */
+const west = ["Nigeria", "Cameroon", "Ghana"];
+console.log(...west);
+
+//Expected Result:  "Nigeria", "Cameroon", "Ghana"
+
+
